90ba6ba89f1c2c14455230dfc28c8a971f85a50b
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-26 15:10:05 -0600

    create a links and add and run components

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,25 +1,45 @@
 import react from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import About from "./components/about.component";
 import Education from "./components/education.component";
 import Experience from "./components/experience.component";
 import Skills from "./components/skills.component";
+import Photo from "./photo.jpg";
 import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <div class="flex-container">
-        <div class="flex-child green">
-          <img alt="" src=".../public/logo192.png" />
-          <h2>hey</h2>
+      <div className="flex-container">
+        <div className="flex-child green">
+          <img src={Photo} alt="myphoto" />
+          <div className="linkdiv">
+          <Router>
+            <Link className="links" to="/">
+              ABOUT
+            </Link>
+            <Link className="links" to="/education">
+              EDUCATION
+            </Link>
+            <Link className="links" to="/experience">
+              EXPERIENCE
+            </Link>
+            <Link className="links" to="/Skills">
+              SKILLS
+            </Link>
+          </Router>
+
+          </div>
+         
         </div>
 
-        <div class="flex-child magenta">
-          <About/>
-          <Education/>
-          <Experience/>
-          <Skills/>
+        <div className="flex-child magenta">
+          <About />
+          <Education />
+          <Experience />
+          <Skills />
         </div>
       </div>
     </div>
