576f2bc34c5d3ca3ed895e0a5c2c221c4c39dda7
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-26 11:00:56 -0600

    Import all components and display them

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import react from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/about.component";
+import Education from "./components/education.component";
+import Experience from "./components/experience.component";
+import Skills from "./components/skills.component";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <div class="flex-container">
+        <div class="flex-child green">
+          <img alt="" src=".../public/logo192.png" />
+          <h2>hey</h2>
+        </div>
+
+        <div class="flex-child magenta">
+          <About/>
+          <Education/>
+          <Experience/>
+          <Skills/>
+        </div>
+      </div>
     </div>
   );
 }
