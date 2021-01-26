924e6ecf018bfee4b37bd0121974e9c0370fa5f3
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-26 10:58:55 -0600

    Create template

---
+++ frontend/portfolio/src/components/experience.component.jsx
@@ -0,0 +1,9 @@
+import react , { Component } from 'react'
+
+export default class Experience extends Component {
+    render() {
+        return (
+            <h1>This is Experience page</h1>
+        )
+    }
+}
---
+++ frontend/portfolio/src/components/skills.component.jsx
@@ -0,0 +1,9 @@
+import react , { Component } from 'react'
+
+export default class Skills extends Component {
+    render() {
+        return (
+            <h1>This is skills page</h1>
+        )
+    }
+}
