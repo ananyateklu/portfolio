4a728f4af660e74b51daa098e5bc097806bc8e1d
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-26 15:37:55 -0600

    Adjusted some styling stuff (capitalize, moving-around, color change)

--- frontend/portfolio/src/components/about.component.jsx
+++ frontend/portfolio/src/components/about.component.jsx
@@ -6,9 +6,13 @@ export default class About extends Component {
     render() {
         return (
             <div className="About">
-                <h1>Ananya </h1>
-                <h1 className="last">Teklu</h1>
-                <h3>2735 Rice Street, Roseville, MN 55113 </h3>
+                <h1>ANANYA </h1>
+                <h1 className="last">TEKLU</h1>
+                <div>
+                <h3>2735 RICE STREET, ROSEVILLE, MN 55113 · (517) 505-1226 · </h3>
+                <h4 className="Email">ANANYA.TEKLU@GMAIL.COM</h4>
+                </div>
+                
 
             </div>
         )
