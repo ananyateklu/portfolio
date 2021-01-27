b9c7bdea3522a70ddfddb3b7b9b2f6e3ae5f35ad
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-26 19:10:09 -0600

    add horizontal line (hr)

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -37,8 +37,11 @@ function App() {
 
         <div className="flex-child magenta">
           <About />
+          <hr/>
           <Education />
+          <hr/>
           <Experience />
+          <hr/>
           <Skills />
         </div>
       </div>
