468858c8aa35a36fba2d1a3c2d6f2b7107b0e11d
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-27 12:09:06 -0600

    Change experience to projects

--- frontend/portfolio/src/components/projects.component.jsx
+++ frontend/portfolio/src/components/projects.component.jsx
@@ -5,7 +5,13 @@ export default class Experience extends Component {
   render() {
     return (
       <div className="Experience">
-        <h1>EXPERIENCE</h1>
+        <h1>PROJECTS</h1>
+        <div>
+          <div><p>a</p></div>
+          <div><p>b</p></div>
+          <div><p>c</p></div>
+          <div><p>d</p></div>
+        </div>
       </div>
     );
   }
