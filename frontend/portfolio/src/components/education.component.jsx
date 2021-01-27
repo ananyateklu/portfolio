54046000aa6a79a6a0d410b9872fba2678ff64c6
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-26 19:11:23 -0600

    Education component is almost done

--- frontend/portfolio/src/components/education.component.jsx
+++ frontend/portfolio/src/components/education.component.jsx
@@ -6,8 +6,15 @@ export default class Education extends Component {
     render() {
         return (
             <div className="Education">
-            <h1>abcd</h1>
-            <h3>2735 Rice Street, Roseville, MN 55113 </h3>
+            <h1>EDUCATION</h1>
+            <div className="work">
+            <h3 >UNIVERSITY OF MINNESOTA MORRIS</h3>
+            <h4> August 2017  -  December 2020</h4>
+            </div>
+            <h3 className="bach">BACHELOR OF ARTS</h3>
+            <h5>Computer Science</h5>
+            <h5> Cumulative GPA: 3.4</h5>
+            <h5> Major GPA: 3.7</h5>
 
         </div>
         )
