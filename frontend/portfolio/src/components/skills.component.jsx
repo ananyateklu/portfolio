0c1d4d6cdb78a51d545563e2087563cee6c5f05c
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-27 22:48:13 -0600

    Add icons for the tools

--- frontend/portfolio/src/components/skills.component.jsx
+++ frontend/portfolio/src/components/skills.component.jsx
@@ -1,16 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import "./components.css";
+import { SiIntellijidea, SiGradle,SiYarn, SiMongodb, SiTravisci, SiMysql, SiHtml5,  } from "react-icons/si"
+import { FaAngular, FaJava, FaReact, FaNode, FaCss3Alt} from "react-icons/fa"
+import { DiSpark, DiNpm } from "react-icons/di"
+import { GoMarkGithub } from "react-icons/go"
+import { IoLogoJavascript } from "react-icons/io"
+
 
 export default class Skills extends Component {
   render() {
     return (
       <div id="skills" className="Skills">
         <h1>SKILLS</h1>
+        <h3>PROGRAMMING LANGUAGES AND TOOLS</h3>
         <div>
-          <div><p>a</p></div>
-          <div><p>b</p></div>
-          <div><p>c</p></div>
-          <div><p>d</p></div>
+          <div className="mainSkill"> 
+            <div className="skillz">< FaAngular size="50px"/></div>
+            <div className="skillz"><FaJava size="50px"/></div>
+            <div className="skillz"><SiIntellijidea size="50px"/></div>
+            <div className="skillz"> <SiGradle size="50px"/></div>
+            <div className="skillz"><SiYarn size="50px"/></div>
+            <div className="skillz"> <SiMongodb size="50px"/></div>
+            <div className="skillz"><SiTravisci size="50px"/></div>
+            <div className="skillz"> <FaReact size="50px"/></div>
+            <div className="skillz"><FaNode size="50px"/></div>
+            <div className="skillz"> <GoMarkGithub size="50px"/></div>
+            <div className="skillz"><SiMysql size="50px"/></div>
+            <div className="skillz"> <SiHtml5 size="50px"/></div>
+            <div className="skillz"> <FaCss3Alt size="50px"/></div>
+            <div className="skillz"> <DiSpark size="50px"/></div>
+            <div className="skillz"> <DiNpm size="50px"/></div>
+           
+        </div>
+          
         </div>
       </div>
     );
