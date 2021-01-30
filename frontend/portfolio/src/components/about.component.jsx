41c6e25d0dc9dd4060173a9d6a5be3b208f37269
Ananya Teklu <teklu009@morris.umn.edu>
2021-01-30 14:58:44 -0600

    Update about me

--- frontend/portfolio/src/components/about.component.jsx
+++ frontend/portfolio/src/components/about.component.jsx
@@ -16,7 +16,9 @@ export default class About extends Component {
           <h3>2735 RICE STREET, ROSEVILLE, MN 55113 · (517) 505-1226 · </h3>
           <h4 className="Email">ANANYA.TEKLU@GMAIL.COM</h4>
           <h5>
-          Software Developer skilled at technical leadership, communication and presentations. Experienced in full project life cycle from design to implementation to integration.
+          Versatile Software developer introduces interpersonal communication and understanding of 
+          web development considerations. Excellent organizational skills. Clear understanding of HTML, CSS and JavaScript and training in React. 
+          Skilled at designing and developing sites from concept to roll out. Versatile understanding of complex backend data functions and analytics.
           </h5>
           <div className="Icondiv"><a href="https://github.com/ananyateklu"><GitHubIcon/></a></div>
           <div className="Icondiv"><a href="https://www.linkedin.com/in/ananya-teklu-37b5b2154/"><LinkedInIcon/></a></div>
